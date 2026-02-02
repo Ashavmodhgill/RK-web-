1dee37b54f2a58a070e81a362fe1fd9eeff2cca5
Ashavmodhgill <deepmodhgill9@gmail.com>
2026-02-02 22:34:03 +0530

    Setting Up the server and installed mongoose

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import {connect} from '../src/config/database.js'
+import bodyParser from 'body-parser';
+
+const app = express();
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({ extended: true}));
+
+app.listen(3003, async()=>{
+console.log("server started");
+ await connect();
+ console.log("DB connected")
+
+})
